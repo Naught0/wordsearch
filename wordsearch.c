31ced774f63cb64a5304ae0b0112e9e60d712257
james <naughtsee@gmail.com>
2015-02-25 22:39:25 -0500

    upd8s

--- wordsearch.c
+++ wordsearch.c
@@ -17,6 +17,13 @@ void getWords(int, word[]);
 int randNum(int);
 void revStr(char[], char[]);
 void blankStr(char[]);
+void displayBoard(int s, char wordSearch[s][s]);
+
+// Checks to see if you can lay the word horizontally
+int checkHoriz(int s, char wordSearch[s][s], char[], int *x, int *y);
+void placeHoriz(int s, char wordSearch[s][s], char word[], int x, int y);
+int checkVert(int s, char wordSearch[s][s], char[], int *x, int *y);
+void placeVert(int s, char wordSearch[s][s], char word[], int x, int y);
 
 int main(){
     // seed random #
@@ -45,34 +52,73 @@ int main(){
     genArray(boardSize, wordSearch);
 
     // place words in array
-    	// Decide horizontal, vertical, backwards
     for(int i = 0; i < numWords; i++){
+        int initx = 0, inity = 0;
+        // Decide horizontal, vertical, backwards
         int decide = randNum(2);
-
-        switch(decide){
-        	case 0:
-            case 1:
-                int ranX = randNum(boardSize - 1);
-                int ranY = randNum(boardSize - 1);
-                while(wordSearch[ranX][ranY] == '\0'){                    
-                    for(int j = 0; j < strlen(wordBank[i].word); j++){
-
-                    }
+            if(decide == 0 || decide == 2){
+                while(checkHoriz(boardSize, wordSearch, wordBank[i].word, &initx, &inity)){
+                    placeHoriz(boardSize, wordSearch, wordBank[i].word, initx, inity);
+                    printf("Placed!\n");
+                    break;
                 }
-        		break;
-        	case 2:
+            }
+            if(decide == 1){
+                while(checkVert(boardSize, wordSearch, wordBank[i].word, &initx, &inity)){
+                    placeVert(boardSize, wordSearch, wordBank[i].word, initx, inity);
+                    printf("Placed!\n");
+                    break;
+                }
+            }
+            // Reverse
+                /*
         		revStr(wordBank[i].word, tempStr);
-                blankStr(tempStr);
-        		break;
-        	default:
-        		break;
-        }
+                blankStr(tempStr);*/
     }
+    displayBoard(boardSize, wordSearch);
     	// transform if needed
     	// place
 
-
 }
+
+void placeHoriz(int s, char wordSearch[s][s], char word[], int x, int y){
+    int len = strlen(word);
+    for(int i = 0; i < len; i++){
+        wordSearch[x][y + i] = word[i];
+    }
+}
+
+int checkHoriz(int s, char wordSearch[s][s], char word[], int *x, int *y){
+    *x = randNum(s);
+    *y = randNum(s);
+    int len = strlen(word);
+    for(int i = 0; i < len; i++){ 
+        if(wordSearch[*x][*y + i] != '.'){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void placeVert(int s, char wordSearch[s][s], char word[], int x, int y){    
+    int len = strlen(word);
+    for(int i = 0; i < len; i++){
+        wordSearch[x + i][y] = word[i];
+    }
+}
+
+int checkVert(int s, char wordSearch[s][s], char word[], int *x, int *y){
+    *x = randNum(s);
+    *y = randNum(s);
+    int len = strlen(word);
+    for(int i = 0; i < len; i++){ 
+        if(wordSearch[*x + i][*y] != '.'){
+            return 0;
+        }
+    }
+    return 1;
+}
+
 
 void welcome(){
     printf("This program will generate a word search based on user input.\n\n");
@@ -106,7 +152,7 @@ void getWords(int numWords, word wordBank[]){
 void genArray(int aSize, char arr[aSize][aSize]){
 	for(int i = 0; i < aSize; i++){
 		for(int j = 0; j < aSize; j++){
-			arr[i][j] = '\0';
+			arr[i][j] = '.';
 		}
 	}
 }
@@ -134,4 +180,13 @@ void blankStr(char str[]){
     for(int i = 0; i < STRLEN; i++){
         str[i] = '\0';
     }
+}
+
+void displayBoard(int s, char wordSearch[s][s]){
+    for (int i = 0; i < s; i++) {
+        for (int j = 0; j < s; j++) {
+            printf("%c ", wordSearch[i][j]);
+        }
+        printf("\n");
+    } 
 }
